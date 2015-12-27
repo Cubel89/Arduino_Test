adbdf3a499ae5c12259d773f92b4804d5fcd8d66
Cubel89 <p.cubel@gmail.com>
2015-12-27 17:29:11 +0100

    Algunas pruebas realizadas (Nada funcional)

--- app/src/main/java/es/ateneasystems/arduinotest/fragments/Home.java
+++ app/src/main/java/es/ateneasystems/arduinotest/fragments/Home.java
@@ -1,9 +1,16 @@
 package es.ateneasystems.arduinotest.fragments;
 
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +19,14 @@ import es.ateneasystems.arduinotest.R;
 
 
 public class Home extends Fragment {
+    /**
+     * Log
+     */
+    private String logname = "Home (Fragment)";
+
+
+    //Variables
+    private View view;
 
     public Home() {
         // Required empty public constructor
@@ -22,9 +37,54 @@ public class Home extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        view = inflater.inflate(R.layout.fragment_home, container, false);
 
-        //Al final del tot retornamos el view
+        //Declaracion de componentes
+        FloatingActionButton fab = (FloatingActionButton) view.findViewById(R.id.fab);
+
+
+
+        //Pulsaciones de botones
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mostrar_cartel("Botón no funcional");
+                createSimpleDialog("Permisos Bluetooth","Es necesario activar el bluetooth para poder buscar otros dispositovos");
+            }
+        });
+
+        //Devolvemos la vista
         return view;
+        };
+
+    public void mostrar_cartel (String texto){
+        Snackbar.make(view, texto, Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
     }
-}
+
+    public AlertDialog createSimpleDialog(String titulo, String mensaje) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        builder.setTitle(titulo)
+                .setMessage(mensaje)
+                .setPositiveButton("OK",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                //listener.onPossitiveButtonClick();
+                                Log.d(logname,"OK");
+                            }
+                        })
+                .setNegativeButton("CANCELAR",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                //listener.onNegativeButtonClick();
+                                Log.d(logname,"KO");
+                            }
+                        });
+
+        return builder.create();
+    }
+
+};
